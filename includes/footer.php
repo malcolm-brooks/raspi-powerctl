f771a720399e0df052cc231d9ee224e2ee6276d9
Malcolm Brooks <mail@malcolmbrooks.co.uk>
2017-06-11 07:35:26 +0000

    Remove redundant container from footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,9 @@
 		<footer class="footer">
-			<div class="container">
-			<?php
-				$config = json_decode(file_get_contents("config.json", true));
-				$footer = $config->site->footer;
-				echo "<p class=\"text-muted\">$footer</p>";
-			?>
-			</div>
+		<?php
+			$config = json_decode(file_get_contents("config.json", true));
+			$footer = $config->site->footer;
+			echo "<p class=\"text-muted\">$footer</p>";
+		?>
 		</footer>
 	</body>
 </html>
